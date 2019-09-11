c96e208f4adb86f63d9502d755cbceb8ec423ad8
msapna717 <46435843+msapna717@users.noreply.github.com>
2019-09-11 17:23:37 +0530

    code extra space removed

--- src/testProj/PalindromNumber.java
+++ src/testProj/PalindromNumber.java
@@ -3,36 +3,36 @@ package testProj;
 import java.util.Scanner;
 
 public class PalindromNumber {
-	static int reverse=0;
-	static int number,n;
+	static int reverse = 0;
+	static int number, n;
 	static int temp;
+
 	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		System.out.println("Input number: ");
-		 number=in.nextInt();
-		System.out.println("You have entered number:"+number);
+		number = in.nextInt();
+		System.out.println("You have entered number:" + number);
 		reverseNumber();
 		IsPalindromNumbers();
-		
-		
-		
+
 	}
-	 static void reverseNumber() {
-		 n=number;
-		while (n>0) {
-		temp=n%10;
-		n=n/10;
-		reverse=(reverse*10)+temp;
+
+	static void reverseNumber() {
+		n = number;
+		while (n > 0) {
+			temp = n % 10;
+			n = n / 10;
+			reverse = (reverse * 10) + temp;
 		}
+	}
+
+	public static void IsPalindromNumbers() {
+		System.out.println("Reverse of " + number + " is: " + reverse);
+		if (number == reverse) {
+			System.out.println(number + " is palidrome number");
+		} else {
+			System.out.println(number + " is not palidrome number");
 		}
-public static void IsPalindromNumbers() {
-	System.out.println("Reverse of "+number+" is: "+reverse);
-	if(number==reverse) {
-		System.out.println(number+" is palidrome number");
+
 	}
-	else {
-		System.out.println(number+" is not palidrome number");
-	}
-	
-}
 }
